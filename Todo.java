775762a0a4f5b4ced21724e29bea8ba11e218256
Jack Branca <jackbranca00@gmail.com>
2019-02-07 14:17:25 -0600

    First full test case made in AnimalTest, Todo moved to top of directory

---
+++ Todo.java
@@ -0,0 +1,21 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-07
+OU ID_______________: 113480507
+OU EMAIL____________: John.r.branca-1@ou.edu
+
+Tasks:
+1. Finish Animal Equals Method
+2. Finish Equipment Equals Method
+3. Figure out how github works
+
+Estimated Time (minutes):
+1. 30
+2. 30
+3. 45
+
+Actual Time (minutes):
+1. 30
+2. 35
+3. 90
+*/}
